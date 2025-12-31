0b1f4a5e262c03f7caf96b97219c903c0ac2d7cd
ngoctamlam2612 <ngoctam.lam2612@gmail.com>
2025-12-31 13:31:35 +0700

    feat(context)[phase-5]: Add ApiContextBuilder for assembling ApiContext

--- UnifiedTestingFramework/src/main/java/core/context/api/ApiContextBuilder.java
+++ UnifiedTestingFramework/src/main/java/core/context/api/ApiContextBuilder.java
@@ -1,5 +1,51 @@
 package core.context.api;
 
+import core.context.ContextException;
+import core.context.adapter.ResponseAdapter;
+
+/**
+ * Builder responsible for assembling ApiContext.
+ *
+ * Responsibilities:
+ * - Build pattern for API context
+ * - Set response adapter
+ * - Validate required components
+ * - Construct ApiContext in a controlled way
+ *
+ */
 public class ApiContextBuilder {
-    
+    /**
+     * Normalized response adapter.
+     */
+    private ResponseAdapter responseAdapter;
+
+    /**
+     * Bind response adapter to this builder.
+     *
+     * @param adapter normalized response adapter
+     * @return builder itself (fluent API)
+     */
+    public ApiContextBuilder withResponseAdapter(ResponseAdapter adapter) {
+        // Store response adapter
+        this.responseAdapter = adapter;
+
+        // Return builder
+        return this;
+    }
+
+    /**
+     * Builds the APIContext instance.
+     *
+     * @return fully initialized ApiContext
+     * @throws ContextException/IllegalStateException if required components are missing
+     */
+    public ApiContext build() {
+        // Fail fast if context is misconfigured
+        if (responseAdapter == null) {
+            throw new ContextException( "ResponseAdapter must be provided before building ApiContext");
+        }
+
+        //Return new DefaultApiContext
+        return new DefaultApiContext(responseAdapter);
+    }
 }
