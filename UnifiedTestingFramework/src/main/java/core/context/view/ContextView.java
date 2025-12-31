fa404073aa4509744a63c6e60252efb51134b89a
ngoctamlam2612 <ngoctam.lam2612@gmail.com>
2025-12-31 20:17:06 +0700

    refactor(context)[phase-4]: Remove summary() from ContextView

--- UnifiedTestingFramework/src/main/java/core/context/view/ContextView.java
+++ UnifiedTestingFramework/src/main/java/core/context/view/ContextView.java
@@ -7,14 +7,4 @@ package core.context.view;
  * - Expose data in a test-friendly, assertable format
  * - Immutable / read-only
  */
-public interface ContextView {
-    /**
-     * Optionally, return a descriptive summary of this view.
-     *
-     * @return a concise string summary of the context view
-     */
-    default String summary() {
-        // Default implementation returns toString
-        return this.toString();
-    }
-}
+public interface ContextView {}
