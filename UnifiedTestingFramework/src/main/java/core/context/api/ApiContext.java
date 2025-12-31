91898e9a93294641aa477d82f151851dcd9a7d38
ngoctamlam2612 <ngoctam.lam2612@gmail.com>
2025-12-31 12:53:01 +0700

    feat(context)[phase-5]: Add ApiContext as API-specific  context interface

--- UnifiedTestingFramework/src/main/java/core/context/api/ApiContext.java
+++ UnifiedTestingFramework/src/main/java/core/context/api/ApiContext.java
@@ -1,5 +1,20 @@
 package core.context.api;
 
-public class ApiContext {
-    
+import core.context.adapter.ResponseAdapter;
+
+/**
+ * API-specific context contract.
+ *
+ * Responsibilities:
+ * - Represents an API execution context in a test
+ * - Exposes API-related data in a normalized way
+ */
+public interface ApiContext {
+    /**
+     * Get the ResponseAdapter instance associated with this API context.
+     * The ResponseAdapter holds the HTTP response body and headers.
+     *
+     * @return the ResponseAdapter normalized API response adapter (tool-agnostic, read-only)t
+     */
+    ResponseAdapter response();
 }
